afc079586650afce090222fe855a4fbfa6b0c624
zaephyr <uros.mahne@gmail.com>
2020-10-21 15:03:35 +0200

    App moved into github

---
+++ server.js
@@ -0,0 +1,39 @@
+//Import the mongoose module
+var mongoose = require('mongoose');
+const dotenv = require('dotenv');
+
+process.on('uncaughtException', (err) => {
+    console.log('UNCAUGHT EXCEPTION:< Shutting down..');
+    console.log(err.name, err.message);
+    process.exit(1);
+});
+
+dotenv.config({ path: './config.env' });
+const app = require('./app');
+
+// const DB = process.env.DATABASE.replace('<PASSWORD>', process.env.DATABASE_PASSWORD);
+// //Set up default mongoose connection
+
+// mongoose
+//     .connect(DB, {
+//         useNewUrlParser: true,
+//         useCreateIndex: true,
+//         useFindAndModify: false,
+//         useUnifiedTopology: true,
+//     })
+//     .then(() => {
+//         console.log('DB connection successful!');
+//     });
+
+const port = process.env.PORT || 3000;
+const server = app.listen(3000, () => {
+    console.log(`App running on port ${port}...`);
+});
+
+// process.on('unhandledRejection', (err) => {
+//     console.log(err.name, err.message);
+//     console.log('UNHANDLER REJECTION :< Shutting down..');
+//     server.close(() => {
+//         process.exit(1);
+//     });
+// });
